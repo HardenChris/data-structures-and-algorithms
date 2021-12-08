163191044e29dc94e276074f88798b29e9b7ba6d
Christopher Harden <Harden.Christopher00@gmail.com>
2021-12-08 13:20:29 -0600

    fixed merge issue

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -258,11 +258,7 @@ describe('Testing challenge 5', () => {
   });
 });
 
-<<<<<<< HEAD
 xdescribe('Testing challenge 6', () => {
-=======
-describe('Testing challenge 6', () => {
->>>>>>> value-vs-reference
   test('It should return an array containing the stats that are greater than the input', () => {
     expect(getBaseStatGreaterThan(snorlaxData.stats, 75)).toStrictEqual([ { stat: { url: 'https://pokeapi.co/api/v2/stat/5/', name: 'special-defense' }, effort: 2, baseStat: 110 } ]);
     expect(getBaseStatGreaterThan(snorlaxData.stats, 75).length).toStrictEqual(1);
